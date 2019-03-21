4abc023436c55b593cfc115896c1c52ec75bad2a
joe bloggs <joe bloggs@gmail.com>
2019-03-21 16:13:14 +0000

    updating index page

--- src/main/java/com/affablebean/controller/MainController.java
+++ src/main/java/com/affablebean/controller/MainController.java
@@ -1,18 +1,27 @@
 package com.affablebean.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.affablebean.repository.CategoryRepository;
 
 @Controller
 public class MainController {
 
+	@Resource
+	private CategoryRepository categoryRepository;
+	
 	@GetMapping({ "/contact" })
 	public String contact() {
 		return "contact";
 	}
 
 	@GetMapping({ "/", "/index" })
-	public String index() {
+    public String index(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
 		return "index";
 	}
 
