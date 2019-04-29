191061d96ef41285582159fab32a613fe1de2a4d
joe bloggs <joe bloggs@gmail.com>
2019-04-29 12:33:13 +0100

    home page tests

--- react/src/containers/Home/Home.test.js
+++ react/src/containers/Home/Home.test.js
@@ -4,10 +4,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { act } from "react-dom/test-utils";
 import { configureStore } from "redux-starter-kit";
 import { Provider } from "react-redux";
-import Home from "../containers/Home";
-import reducer from "../reducers";
+import Home from ".";
+import reducer from "../../reducers";
 
-jest.mock("../net/categories");
+jest.mock("../../net/categories");
 
 let container;
 
@@ -38,20 +38,23 @@ it("loads a category product details", () => {
     ReactDOM.render(home, container);
   });
 
-  const cat = container.querySelector(".categoryImage");
+  const categories = container.querySelectorAll(".categoryBox");
+  expect(categories.length).toBe(6);
+
+  const greeting = document.querySelector("p");
+  expect(greeting.textContent).toBe(
+    "Welcome to the online home of the Affable Bean Green Grocer."
+  );
+
+  const headerLogo = document.querySelector("#logoText");
+  expect(headerLogo.alt).toBe("the affable bean");
+
+  const footerButtons = document.querySelectorAll(".btn-link");
+  expect(footerButtons.length).toBe(2);
+
+  const category = container.querySelector(".categoryImage");
 
   act(() => {
-    cat.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    category.dispatchEvent(new MouseEvent("click", { bubbles: true }));
   });
-
-  // const label = container.querySelector('p');
-  // expect(label.textContent).toBe('You clicked 0 times');
-  // expect(document.title).toBe('You clicked 0 times');
-
-  // // Test second render and componentDidUpdate
-  // act(() => {
-  //   button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-  // });
-  // expect(label.textContent).toBe('You clicked 1 times');
-  // expect(document.title).toBe('You clicked 1 times');
 });
