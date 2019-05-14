c78c4ad725d174fc36ba968713b59933d110c52d
joe bloggs <joe bloggs@gmail.com>
2019-05-14 09:42:14 +0100

    native checkout

--- native/src/components/CheckoutForm/index.js
+++ native/src/components/CheckoutForm/index.js
@@ -23,8 +23,7 @@ export default class CheckoutForm extends Component {
     });
   };
 
-  submitForm = () => {
-    event.preventDefault();
+  purchaseItems = () => {
     const { dispatch, cart } = this.props;
     let validForm = true;
 
@@ -66,15 +65,15 @@ export default class CheckoutForm extends Component {
       textInput: {
         borderBottomColor: "black",
         borderBottomWidth: 1,
-        padding: 16
+        padding: 8
       }
     });
 
     return (
       <View>
         <Text style={{ padding: 8 }} />
-        <View style={{ backgroundColor: "#f7f7e9" }}>
-          <Text style={{ padding: 16 }}>
+        <View style={{ backgroundColor: "#f7f7e9", padding: 16 }}>
+          <Text>
             In order to purchase the items in your shopping cart, please provide
             us with the following information:
           </Text>
@@ -84,162 +83,43 @@ export default class CheckoutForm extends Component {
             style={styles.textInput}
             value={this.state.name1}
           />
+          <TextInput
+            onChangeText={email => this.onChange("email", email)}
+            placeholder="Enter your email address"
+            style={styles.textInput}
+            value={this.state.email}
+          />
+          <TextInput
+            onChangeText={phone => this.onChange("phone", phone)}
+            placeholder="Enter your phone number"
+            style={styles.textInput}
+            value={this.state.phone}
+          />
+          <TextInput
+            onChangeText={address => this.onChange("address", address)}
+            placeholder="Enter your home address"
+            style={styles.textInput}
+            value={this.state.address}
+          />
+          <TextInput
+            onChangeText={creditcard => this.onChange("creditcard", creditcard)}
+            placeholder="Enter your credit card number"
+            style={styles.textInput}
+            value={this.state.creditcard}
+          />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ paddingTop: 16 }}>
+            Subtotal: &euro; {cart.subtotal.toFixed(2)}
+          </Text>
+          <Text>Surcharge: &euro; {surcharge.toFixed(2)}</Text>
+          <Text style={{ fontWeight: "bold" }}>
+            Total: &euro; {(cart.subtotal + surcharge).toFixed(2)}{" "}
+          </Text>
+          <Text style={{ marginTop: 8 }} />
+          <Button onPress={this.purchaseItems} title="Purchase" />
         </View>
       </View>
-      //   <div className="singleColumn">
-      //     <h2>checkout</h2>
-      //     <p>
-      //       In order to purchase the items in your shopping cart, please provide
-      //       us with the following information:
-      //     </p>
-      //     <br />
-      //     <form className="form-horizontal" onSubmit={this.handleSubmit}>
-      //       <div className="form-group">
-      //         <label htmlFor="name" className={`col-sm-2 control-label`}>
-      //           name
-      //         </label>
-      //         <div className="col-sm-10">
-      //           <input
-      //             ref={this.nameInputRef}
-      //             type="text"
-      //             className="form-control"
-      //             name="name1"
-      //             maxLength="45"
-      //             placeholder="At least 8 chars and no more than 45 chars"
-      //             size="31"
-      //             onChange={this.handleChange}
-      //             value={this.state.name}
-      //           />
-      //         </div>
-      //         <div className="formError" ref={this.nameErrorRef}>
-      //           Name shoud be at least 8 chars and no more than 45 chars
-      //         </div>
-      //       </div>
-      //       <div className="form-group">
-      //         <label htmlFor="email" className={`col-sm-2 control-label`}>
-      //           email
-      //         </label>
-      //         <div className="col-sm-10">
-      //           <input
-      //             ref={this.emailInputRef}
-      //             type="email"
-      //             className="form-control"
-      //             name="email"
-      //             maxLength="45"
-      //             placeholder="At least 8 chars and no more than 45 chars"
-      //             size="31"
-      //             onChange={this.handleChange}
-      //             value={this.state.email}
-      //           />
-      //         </div>
-      //         <div className="formError" ref={this.emailErrorRef}>
-      //           Email shoud be at least 8 chars and no more than 45 chars
-      //         </div>
-      //       </div>
-      //       <div className="form-group">
-      //         <label htmlFor="phone" className={`col-sm-2 control-label`}>
-      //           phone
-      //         </label>
-      //         <div className="col-sm-10">
-      //           <input
-      //             ref={this.phoneInputRef}
-      //             type="text"
-      //             className="form-control"
-      //             name="phone"
-      //             maxLength="45"
-      //             placeholder="At least 8 chars and no more than 30 chars"
-      //             size="31"
-      //             onChange={this.handleChange}
-      //             value={this.state.phone}
-      //           />
-      //         </div>
-      //         <div className="formError" ref={this.phoneErrorRef}>
-      //           Phone shoud be at least 8 chars and no more than 30 chars
-      //         </div>
-      //       </div>
-      //       <div className="form-group">
-      //         <label htmlFor="address" className={`col-sm-2 control-label`}>
-      //           address
-      //         </label>
-      //         <div className="col-sm-10">
-      //           <input
-      //             ref={this.addressInputRef}
-      //             type="text"
-      //             className="form-control"
-      //             name="address"
-      //             maxLength="45"
-      //             placeholder="At least 8 chars and no more than 45 chars"
-      //             size="31"
-      //             onChange={this.handleChange}
-      //             value={this.state.address}
-      //           />
-      //         </div>
-      //         <div className="formError" ref={this.addressErrorRef}>
-      //           Address shoud be at least 8 chars and no more than 45 chars
-      //         </div>
-      //       </div>
-      //       <div className="form-group">
-      //         <label htmlFor="creditcard" className={`col-sm-2 control-label`}>
-      //           credit card
-      //         </label>
-      //         <div className="col-sm-10">
-      //           <input
-      //             ref={this.ccInputRef}
-      //             type="text"
-      //             className="form-control"
-      //             name="creditcard"
-      //             maxLength="45"
-      //             placeholder="At least 16 chars and no more than 19 chars"
-      //             size="31"
-      //             onChange={this.handleChange}
-      //             value={this.state.creditcard}
-      //           />
-      //         </div>
-      //         <div className="formError" ref={this.ccErrorRef}>
-      //           Credit card number shoud be at least 16 chars and no more than 19
-      //           chars
-      //         </div>
-      //       </div>
-      //       <div className="form-group">
-      //         <div className={`col-sm-offset-2 col-sm-10`}>
-      //           <button type="submit" className={`btn btn-primary`}>
-      //             submit purchase
-      //           </button>
-      //         </div>
-      //       </div>
-      //     </form>
-      //     <InfoBox>
-      //       <ul>
-      //         <li>Next-day delivery is guaranteed</li>
-      //         <li>
-      //           A &euro; {surcharge.toFixed(2)} delivery surcharge is applied to
-      //           all purchase orders
-      //         </li>
-      //       </ul>
-      //       <PriceBox>
-      //         <tbody>
-      //           <tr>
-      //             <PriceBoxTd>subtotal:</PriceBoxTd>
-      //             <PriceBoxSubTotalTd className="checkoutPriceColumn">
-      //               &euro; {cart.subtotal.toFixed(2)}
-      //             </PriceBoxSubTotalTd>
-      //           </tr>
-      //           <tr>
-      //             <PriceBoxTd>surcharge:</PriceBoxTd>
-      //             <PriceBoxSubTotalTd className="checkoutPriceColumn">
-      //               &euro; {surcharge.toFixed(2)}
-      //             </PriceBoxSubTotalTd>
-      //           </tr>
-      //           <tr>
-      //             <PriceBoxTd>total:</PriceBoxTd>
-      //             <PriceBoxSubTotalTd className="checkoutPriceColumn">
-      //               &euro; {(cart.subtotal + surcharge).toFixed(2)}
-      //             </PriceBoxSubTotalTd>
-      //           </tr>
-      //         </tbody>
-      //       </PriceBox>
-      //     </InfoBox>
-      //   </div>
     );
   }
 }
