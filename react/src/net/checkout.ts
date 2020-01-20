8fecd332fb2d1a82d9ddce68167921bae910ec07
joe bloggs <joe bloggs@gmail.com>
2020-01-20 09:54:20 +0000

    net axios

--- react/src/net/checkout.ts
+++ react/src/net/checkout.ts
@@ -1,46 +1,24 @@
+import axios from "axios";
 import { getPath } from "../helpers/utils";
 import { clearCart } from "../redux/cart";
 import { orderPurchase } from "../redux/purchase";
 
-export const purchaseOrder = (data: any) => async (dispatch: Function) => {
-  const response = await fetch(getPath("purchase2"), {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    // credentials: "same-origin", // include, *same-origin, omit
+export const purchaseOrder = (data: any) => (dispatch: Function) => {
+  axios({
+    method: "post",
+    url: getPath("purchase2"),
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-    // redirect: "follow", // manual, *follow, error
-    // referrer: "no-referrer", // no-referrer, *client
-  });
-
-  try {
-    const json = await response.json();
-    dispatch(clearCart({}));
-    dispatch(
-      orderPurchase({
-        order: json
-      })
-    );
-  } catch (e) {
-    console.log(e);
-  }
+    data: JSON.stringify(data)
+  })
+    .then(response => {
+      dispatch(clearCart({}));
+      dispatch(
+        orderPurchase({
+          order: response.data
+        })
+      );
+    })
+    .catch(error => console.log(error));
 };
-
-// export const purchaseOrder = (data: any) => (dispatch: Function) => {
-//   axios
-//     .post(getPath("purchase2"))
-//     .then(response => {
-//       dispatch(clearCart({}));
-//       dispatch(
-//         orderPurchase({
-//           order: response.data
-//         })
-//       );
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     });
-// };
