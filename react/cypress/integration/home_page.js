582ad1a26f143362cc6fe70a08aeb2d42721a1dd
joe bloggs <joe bloggs@gmail.com>
2020-01-27 10:34:15 +0000

    init

---
+++ react/cypress/integration/home_page.js
@@ -0,0 +1,12 @@
+describe("The Home Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("contains welcome message", () => {
+    cy.get("p:first").should(
+      "contain",
+      "Welcome to the online home of the Affable Bean Green Grocer."
+    );
+  });
+});
