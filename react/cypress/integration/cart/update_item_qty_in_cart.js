408843a0453c5817a76930a55f0a031024db7f05
joe bloggs <joe bloggs@gmail.com>
2020-01-28 13:52:51 +0000

    refactored e2e tests

--- react/cypress/integration/cart/update_item_qty_in_cart.js
+++ react/cypress/integration/cart/update_item_qty_in_cart.js
@@ -11,6 +11,7 @@ describe("Add and update item quantity in cart", () => {
     cy.get('[data-cy="input-qty-butter"]')
       .type("{uparrow}")
       .should("have.value", "2");
+    // seems to be a bug somewhere in chrome that resets the input number picker back to 1 when clicking form submit
     // cy.get('[data-cy="update-qty-butter"]').click();
   });
 });
