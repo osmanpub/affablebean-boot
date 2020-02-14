f41deea2130d0a23a58c6016dacb29c4c37c754b
joe bloggs <joe bloggs@gmail.com>
2020-02-14 11:43:38 +0000

    fixed e2e tests

--- react/cypress/integration/categories/fruit_veg.spec.js
+++ react/cypress/integration/categories/fruit_veg.spec.js
@@ -1,8 +1,6 @@
 describe("Fruit & Veg Category", () => {
   it("should load all fruit & veg products", () => {
-    cy.get('[data-cy="category-fruit & veg"]').click();
-    cy.get('[data-cy="product-watermelon"] > :nth-child(2)').contains(
-      "watermelon"
-    );
+    cy.get('[data-cy="category-fruit n veg"]').click();
+    cy.get('[data-cy="product-broccoli"] > :nth-child(2)').contains("broccoli");
   });
 });
