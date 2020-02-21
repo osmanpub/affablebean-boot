039898143449f1336cffd06aea6075e73765f0db
joe bloggs <joe bloggs@gmail.com>
2020-02-21 16:28:49 +0000

    category component

--- react/src/components/Category/__tests__/index.test.tsx
+++ react/src/components/Category/__tests__/index.test.tsx
@@ -1,21 +1,35 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import renderer from "react-test-renderer";
 import Category from "..";
-describe("<Category />", () => {
-  const category = {
-    id: 1,
-    name: "dairy"
-  };
 
+const category = {
+  id: 1,
+  name: "dairy"
+};
+
+const categoryComponent = (
+  <Router>
+    <Category category={category} />
+  </Router>
+);
+
+describe("<Category />", () => {
   it("renders correctly", () => {
-    const component = renderer
-      .create(
-        <Router>
-          <Category category={category} />
-        </Router>
-      )
-      .toJSON();
+    const component = renderer.create(categoryComponent).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it("category present", () => {
+    const { getByTestId } = render(categoryComponent);
+    const box = getByTestId(/categoryBox/i);
+    expect(box).toBeInTheDocument();
+  });
+
+  it("is dairy category", () => {
+    const { getByAltText } = render(categoryComponent);
+    const img = getByAltText(/dairy/i);
+    expect(img).toBeInTheDocument();
   });
 });
