38ec1b5e16717e2b61af9c31cff04dd67b6a490d
joe bloggs <joe bloggs@gmail.com>
2020-02-23 09:45:38 +0000

    screens

--- react/src/screens/Home/__tests__/index.test.tsx
+++ react/src/screens/Home/__tests__/index.test.tsx
@@ -1,40 +1,124 @@
-import { container, queries, setupAll } from "../../../setupTests";
+import { render } from "@testing-library/react";
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import Home from "..";
+
+const mockStore = configureStore([thunk]);
+
+const initialState = {
+  categories: {
+    didInvalidate: false,
+    isFetching: false,
+    items: [
+      {
+        id: 1,
+        name: "dairy"
+      },
+      {
+        id: 2,
+        name: "meats"
+      },
+      {
+        id: 3,
+        name: "bakery"
+      },
+      {
+        id: 4,
+        name: "fruit & veg"
+      },
+      {
+        id: 5,
+        name: "cereals"
+      },
+      {
+        id: 6,
+        name: "drinks"
+      }
+    ]
+  }
+};
+
+const store = mockStore(initialState);
+
+const homeScreen = (
+  <Router>
+    <Provider store={store}>
+      <Home match={{ path: "/", url: "/", isExact: true, params: {} }} />
+    </Provider>
+  </Router>
+);
 
 describe("<Home />", () => {
-  setupAll();
+  it("renders correctly", () => {
+    const component = renderer.create(homeScreen).toJSON();
+    // @ts-ignore
+    expect(component).toMatchSnapshot();
+  });
+
+  it("show smaller navigatioal logo, on left", () => {
+    const { getByAltText } = render(homeScreen);
+    const intro = getByAltText(/Affable Bean logo/i);
+    // @ts-ignore
+    expect(intro).toBeInTheDocument();
+  });
+
+  it("show larger logo, on right", () => {
+    const { getByAltText } = render(homeScreen);
+    const intro = getByAltText(/the affable bean/i);
+    // @ts-ignore
+    expect(intro).toBeInTheDocument();
+  });
+
+  it("don't show view cart link", () => {
+    const { getByText } = render(homeScreen);
+    const link = getByText(/view cart/i);
+    // @ts-ignore
+    expect(link).not.toBeVisible();
+  });
+
+  it("don't show proceed to checkout link", () => {
+    const { getByText } = render(homeScreen);
+    const link = getByText(/proceed to checkout/i);
+    // @ts-ignore
+    expect(link).not.toBeVisible();
+  });
+
+  it("don't show cart header total", () => {
+    const { getByTestId } = render(homeScreen);
+    const cart = getByTestId(/cart-hdr-total/i);
+    // @ts-ignore
+    expect(cart).not.toBeVisible();
+  });
 
   it("show welcome paragraph", () => {
-    const { getByTestId } = queries;
+    const { getByTestId } = render(homeScreen);
     const paragraph = getByTestId(/home-welcome/i);
     // @ts-ignore
     expect(paragraph).toBeInTheDocument();
   });
 
-  // it("don't show view cart link", () => {
-  //   const { getByText } = queries;
-  //   const link = getByText(/view cart/i);
-  //   // @ts-ignore
-  //   expect(link).not.toBeVisible();
-  // });
+  it("show six categories", () => {
+    const { getAllByTestId } = render(homeScreen);
+    const boxes = getAllByTestId(/categoryBox/i);
+    // @ts-ignore
+    expect(boxes).toHaveLength(6);
+  });
 
-  // it("renders correctly", () => {
-  //   const greeting = container.querySelector("p");
-  //   // @ts-ignore
-  //   expect(greeting.textContent).toBe(
-  //     "Welcome to the online home of the Affable Bean Green Grocer."
-  //   );
+  it("show privacy", () => {
+    const { getByTestId } = render(homeScreen);
+    const intro = getByTestId(/privacy/i);
+    // @ts-ignore
+    expect(intro).toBeInTheDocument();
+  });
 
-  //   const headerLogo = container.querySelector("#logoText");
-  //   // @ts-ignore
-  //   expect(headerLogo.alt).toBe("the affable bean");
-
-  //   // privacy and contact buttons
-  //   const footerButtons = container.querySelectorAll(".btn-link");
-  //   // @ts-ignore
-  //   expect(footerButtons.length).toBe(2);
-
-  //   const categories = container.querySelectorAll(".categoryBox");
-  //   // @ts-ignore
-  //   expect(categories.length).toBe(6);
-  // });
+  it("show contact", () => {
+    const { getByTestId } = render(homeScreen);
+    const intro = getByTestId(/contact/i);
+    // @ts-ignore
+    expect(intro).toBeInTheDocument();
+  });
 });
--- react/src/setupTests.tsx
+++ react/src/setupTests.tsx
@@ -1,17 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
-import React from "react";
-import ReactTestUtils, { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import reducer from "./redux";
-import Cart from "./screens/Cart";
-import CategoryProducts from "./screens/CategoryProducts";
-import Checkout from "./screens/Checkout";
-import Contact from "./screens/Contact";
-import Home from "./screens/Home";
-import Privacy from "./screens/Privacy";
 
 // Fixes problem - https://stackoverflow.com/questions/48809753/testing-mutationobserver-with-jest
 // @ts-ignore
@@ -22,60 +9,4 @@ global.MutationObserver = class {
   disconnect() {}
   // @ts-ignore
   observe(element, initObject) {}
-};
-
-/**
- * Mock components and modules
- */
-jest.mock("./net/cart");
-jest.mock("./net/category");
-jest.mock("./net/categories");
-jest.mock("./net/checkout");
-jest.mock("./net/subjects");
-
-export let container: any;
-export let queries: any;
-
-export const setupAll = (callback?: Function) =>
-  beforeAll(() => {
-    container = document.createElement("div");
-    document.body.appendChild(container);
-
-    act(() => {
-      const store = configureStore({
-        reducer
-      });
-
-      const app = (
-        <Provider store={store}>
-          <Router>
-            <Route path="/" exact component={Home} />
-            <Route path="/category/:id" component={CategoryProducts} />
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/privacy" component={Privacy} />
-            <Route path="/viewCart/:clear" component={Cart} />
-          </Router>
-        </Provider>
-      );
-
-      queries = render(app);
-    });
-
-    callback && callback();
-  });
-
-export const changeValue = (widget: any, value: any) => {
-  widget.value = value;
-  ReactTestUtils.Simulate.change(widget);
-};
-
-export const mouseClick = (widget: any) =>
-  act(() => {
-    widget.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-  });
-
-export const afterAll = () => {
-  document.body.removeChild(container);
-  container = null;
 };
