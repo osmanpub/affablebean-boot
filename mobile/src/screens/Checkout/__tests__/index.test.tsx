aa68a9167fc1f4b9f1712f22ccb88729b049f8b6
joe bloggs <joe bloggs@gmail.com>
2020-02-25 13:29:21 +0000

    screens

--- mobile/src/screens/Checkout/__tests__/index.test.tsx
+++ mobile/src/screens/Checkout/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import {act, fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import renderer from 'react-test-renderer';
@@ -10,9 +11,20 @@ const initialState = {
   cart: {
     didInvalidate: false,
     isFetching: false,
-    items: [],
-    numberOfItems: 0,
-    subtotal: 0,
+    items: [
+      {
+        product: {
+          id: 1,
+          description: 'semi skimmed (1L)',
+          name: 'milk',
+          price: 1.7,
+        },
+        quantity: 1,
+        total: 1.7,
+      },
+    ],
+    numberOfItems: 1,
+    subtotal: 1.7,
   },
   purchase: {
     didInvalidate: false,
@@ -26,15 +38,126 @@ const initialState = {
 
 const store = mockStore(initialState);
 
+const checkout = (
+  <Provider store={store}>
+    <Checkout setScreen={jest.fn()} />
+  </Provider>
+);
+
 describe('<Checkout />', () => {
   it('renders correctly', () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <Checkout setScreen={jest.fn()} />
-        </Provider>,
-      )
-      .toJSON();
+    const component = renderer.create(checkout).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('show logo', () => {
+    const {getByTestId} = render(checkout);
+    const logo = getByTestId(/logo/i);
+    expect(logo).toBeTruthy();
+  });
+
+  it('show view cart link', () => {
+    const {getByTestId} = render(checkout);
+    const viewCart = getByTestId(/view-cart/i);
+    expect(viewCart).toBeTruthy();
+  });
+
+  it('show clear cart link', () => {
+    const {getByTestId} = render(checkout);
+    const clearCart = getByTestId(/clear-cart/i);
+    expect(clearCart).toBeTruthy();
+  });
+
+  it('show 1 cart items', () => {
+    const {getByText} = render(checkout);
+    const items = getByText(/1 items/i);
+    expect(items).toBeTruthy();
+  });
+});
+
+describe('<Data Entry />', () => {
+  it('renders correctly', () => {
+    const component = renderer.create(checkout).toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('show intro', () => {
+    const {getByTestId} = render(checkout);
+    const intro = getByTestId(/checkout-intro/i);
+    expect(intro).toBeTruthy();
+  });
+
+  it('show 5 input fields', () => {
+    const {getAllByTestId} = render(checkout);
+    const inputs = getAllByTestId('checkout-input');
+    expect(inputs).toHaveLength(5);
+  });
+
+  it('show submit button', () => {
+    const {getByTestId} = render(checkout);
+    const button = getByTestId(/checkout-submit/i);
+    expect(button).toBeTruthy();
+  });
+
+  it("set name to 'osman'", () => {
+    const {getByPlaceholderText} = render(checkout);
+    const input = getByPlaceholderText('Enter your name');
+
+    act(() => {
+      fireEvent.change(input, {nativeEvent: {text: 'osman'}});
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('osman');
+  });
+
+  it("set phone to '1234567890'", () => {
+    const {getByPlaceholderText} = render(checkout);
+    const input = getByPlaceholderText('Enter your phone number');
+
+    act(() => {
+      fireEvent.change(input, {nativeEvent: {text: '1234567890'}});
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('1234567890');
+  });
+
+  it("set email to 'osman@gmail.com'", () => {
+    const {getByPlaceholderText} = render(checkout);
+    const input = getByPlaceholderText('Enter your email address');
+
+    act(() => {
+      fireEvent.change(input, {nativeEvent: {text: 'osman@gmail.com'}});
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('osman@gmail.com');
+  });
+
+  it("set address to 'nobody cares where you live'", () => {
+    const {getByPlaceholderText} = render(checkout);
+    const input = getByPlaceholderText('Enter your address');
+
+    act(() => {
+      fireEvent.change(input, {
+        nativeEvent: {text: 'nobody cares where you live'},
+      });
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('nobody cares where you live');
+  });
+
+  it("set cc to '1234567890123456'", () => {
+    const {getByPlaceholderText} = render(checkout);
+    const input = getByPlaceholderText('Enter your credit card number');
+
+    act(() => {
+      fireEvent.change(input, {nativeEvent: {text: '1234567890123456'}});
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('1234567890123456');
   });
 });
--- mobile/src/screens/Home/__tests__/index.test.tsx
+++ mobile/src/screens/Home/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import {render} from '@testing-library/react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import renderer from 'react-test-renderer';
@@ -8,6 +9,24 @@ import Home from '..';
 const mockStore = configureStore([thunk]);
 
 const initialState = {
+  cart: {
+    didInvalidate: false,
+    isFetching: false,
+    items: [
+      {
+        product: {
+          id: 1,
+          description: 'semi skimmed (1L)',
+          name: 'milk',
+          price: 1.7,
+        },
+        quantity: 1,
+        total: 1.7,
+      },
+    ],
+    numberOfItems: 1,
+    subtotal: 1.7,
+  },
   categories: {
     didInvalidate: false,
     isFetching: false,
@@ -42,15 +61,51 @@ const initialState = {
 
 const store = mockStore(initialState);
 
+const home = (
+  <Provider store={store}>
+    <Home setCategoryProduct={jest.fn()} setScreen={jest.fn()} />
+  </Provider>
+);
+
 describe('<Home />', () => {
   it('renders correctly', () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <Home setCategoryProduct={jest.fn()} setScreen={jest.fn()} />
-        </Provider>,
-      )
-      .toJSON();
+    const component = renderer.create(home).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('show logo', () => {
+    const {getByTestId} = render(home);
+    const logo = getByTestId(/logo/i);
+    expect(logo).toBeTruthy();
+  });
+
+  it('show view cart link', () => {
+    const {getByTestId} = render(home);
+    const viewCart = getByTestId(/view-cart/i);
+    expect(viewCart).toBeTruthy();
+  });
+
+  it('show clear cart link', () => {
+    const {getByTestId} = render(home);
+    const clearCart = getByTestId(/clear-cart/i);
+    expect(clearCart).toBeTruthy();
+  });
+
+  it('show checkout link', () => {
+    const {getByTestId} = render(home);
+    const checkout = getByTestId(/checkout/i);
+    expect(checkout).toBeTruthy();
+  });
+
+  it('show 1 cart items', () => {
+    const {getByText} = render(home);
+    const items = getByText(/1 items/i);
+    expect(items).toBeTruthy();
+  });
+
+  it('show six categories', () => {
+    const {getAllByTestId} = render(home);
+    const boxes = getAllByTestId(/categoryBox/i);
+    expect(boxes).toHaveLength(6);
   });
 });
