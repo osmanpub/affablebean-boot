161e68abd8bb4a082f3876ed2cab7038258a66ab
joe bloggs <joe bloggs@gmail.com>
2020-02-25 12:53:21 +0000

    screens

--- mobile/src/screens/Cart/__tests__/index.test.tsx
+++ mobile/src/screens/Cart/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import {act, fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import renderer from 'react-test-renderer';
@@ -29,15 +30,53 @@ const initialState = {
 
 const store = mockStore(initialState);
 
+const cart = (
+  <Provider store={store}>
+    <Cart setScreen={jest.fn()} />
+  </Provider>
+);
+
 describe('<Cart />', () => {
   it('renders correctly', () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <Cart setScreen={jest.fn()} />
-        </Provider>,
-      )
-      .toJSON();
+    const component = renderer.create(cart).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('show logo', () => {
+    const {getByTestId} = render(cart);
+    const logo = getByTestId(/logo/i);
+    expect(logo).toBeTruthy();
+  });
+
+  it('show clear cart link', () => {
+    const {getByTestId} = render(cart);
+    const clearCart = getByTestId(/clear-cart/i);
+    expect(clearCart).toBeTruthy();
+  });
+
+  it('show checkout link', () => {
+    const {getByTestId} = render(cart);
+    const checkout = getByTestId(/checkout/i);
+    expect(checkout).toBeTruthy();
+  });
+
+  it('show 1 cart items', () => {
+    const {getByText} = render(cart);
+    const items = getByText(/1 items/i);
+    expect(items).toBeTruthy();
+  });
+});
+
+describe('<CartItem />', () => {
+  it('update quantity to 2', () => {
+    const {getByPlaceholderText} = render(cart);
+    const input = getByPlaceholderText('Enter quantity');
+
+    act(() => {
+      fireEvent.change(input, {nativeEvent: {text: '2'}});
+    });
+
+    // @ts-ignore
+    expect(input.props.value).toBe('2');
   });
 });
--- mobile/src/screens/CategoryProducts/__tests__/index.test.tsx
+++ mobile/src/screens/CategoryProducts/__tests__/index.test.tsx
@@ -1,3 +1,4 @@
+import {act, fireEvent, render} from '@testing-library/react-native';
 import React from 'react';
 import {Provider} from 'react-redux';
 import renderer from 'react-test-renderer';
@@ -9,7 +10,22 @@ const mockStore = configureStore([thunk]);
 
 const initialState = {
   cart: {
+    didInvalidate: false,
     isFetching: false,
+    items: [
+      {
+        product: {
+          id: 1,
+          description: 'semi skimmed (1L)',
+          name: 'milk',
+          price: 1.7,
+        },
+        quantity: 1,
+        total: 1.7,
+      },
+    ],
+    numberOfItems: 1,
+    subtotal: 1.7,
   },
   category: {
     categories: {
@@ -79,15 +95,53 @@ const initialState = {
 
 const store = mockStore(initialState);
 
+const categoryProducts = (
+  <Provider store={store}>
+    <CategoryProducts id={1} setScreen={jest.fn()} />
+  </Provider>
+);
+
 describe('<CategoryProducts />', () => {
   it('renders correctly', () => {
-    const component = renderer
-      .create(
-        <Provider store={store}>
-          <CategoryProducts id={1} setScreen={jest.fn()} />
-        </Provider>,
-      )
-      .toJSON();
+    const component = renderer.create(categoryProducts).toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('show logo', () => {
+    const {getByTestId} = render(categoryProducts);
+    const logo = getByTestId(/logo/i);
+    expect(logo).toBeTruthy();
+  });
+
+  it('show clear cart link', () => {
+    const {getByTestId} = render(categoryProducts);
+    const clearCart = getByTestId(/clear-cart/i);
+    expect(clearCart).toBeTruthy();
+  });
+
+  it('show view cart link', () => {
+    const {getByTestId} = render(categoryProducts);
+    const viewCart = getByTestId(/view-cart/i);
+    expect(viewCart).toBeTruthy();
+  });
+
+  it('show checkout link', () => {
+    const {getByTestId} = render(categoryProducts);
+    const checkout = getByTestId(/checkout/i);
+    expect(checkout).toBeTruthy();
+  });
+
+  it('show 1 cart items', () => {
+    const {getByText} = render(categoryProducts);
+    const items = getByText(/1 items/i);
+    expect(items).toBeTruthy();
+  });
+});
+
+describe('<Products />', () => {
+  it('four products are shown', () => {
+    const {getAllByTestId} = render(categoryProducts);
+    const items = getAllByTestId(/product/i);
+    expect(items).toHaveLength(4);
   });
 });
